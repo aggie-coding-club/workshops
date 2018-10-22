e08509636c9835715c8d5bd082db5d394b0dc68f
Gabriel Britain <quontas@gmail.com>
2018-10-22 12:53:46 -0500

    Update and rename create_table.js to create_collection.js

--- api_workshops/create_collection.js
+++ api_workshops/create_collection.js
@@ -1,11 +1,11 @@
 // This is just used to create the collection initially
 
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/";
+let url = "mongodb://localhost:27017/";
 
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
-  var dbo = db.db("tododb");
+  let dbo = db.db("tododb");
   dbo.createCollection("todoLists", function(err, res) {
     if (err) throw err;
     console.log("Collection created!");
