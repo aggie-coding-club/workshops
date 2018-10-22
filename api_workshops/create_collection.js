de7a80c324cd445718e11d3f6722a244bf5943fb
Gabriel Britain <quontas@gmail.com>
2018-10-22 12:58:21 -0500

    Updated README.md to comply with markdown standards

--- api_workshops/create_collection.js
+++ api_workshops/create_collection.js
@@ -1,11 +1,11 @@
 // This is just used to create the collection initially
 
 var MongoClient = require('mongodb').MongoClient
-let url = 'mongodb://localhost:27017/'
+const url = 'mongodb://localhost:27017/'
 
 MongoClient.connect(url, function (err, db) {
   if (err) throw err
-  let dbo = db.db('tododb')
+  const dbo = db.db('tododb')
   dbo.createCollection('todoLists', function (err, res) {
     if (err) throw err
     console.log('Collection created!')
