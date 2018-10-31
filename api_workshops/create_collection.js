27e44879c78c68e2188101c5387529424301d849
shreyshah33 <shreyshah33@gmail.com>
2018-10-30 22:29:25 -0500

    Edited create_collection for GCP

--- api_workshops/create_collection.js
+++ api_workshops/create_collection.js
@@ -1,7 +1,7 @@
 // This is just used to create the collection initially
 
 var MongoClient = require('mongodb').MongoClient
-var password = fs.readFileSync(process.cwd()+"/.env");
+var password = fs.readFileSync('.env');
 var url = `mongodb://acc:${password}@cluster0-shard-00-00-hdiqu.gcp.mongodb.net:27017,cluster0-shard-00-01-hdiqu.gcp.mongodb.net:27017,cluster0-shard-00-02-hdiqu.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true`
 MongoClient.connect(url, { useNewUrlParser: true }, function (err, db) {
   if (err) throw err
