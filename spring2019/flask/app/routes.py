014fb1c438f3aa36b5c9e21678aa4006fb9a284b
Thomas McDonald <thomas.carnegie.1234@gmail.com>
2019-04-01 17:08:23 -0500

    added import for classifier

--- spring2019/flask/app/routes.py
+++ spring2019/flask/app/routes.py
@@ -1,6 +1,9 @@
 from flask import render_template, redirect, url_for
 from app import app
 from app.forms import ChatForm
+from faq import QuestionClassifier
+
+classifier = QuestionClassifier()
 
 @app.route('/')
 @app.route('/home')
