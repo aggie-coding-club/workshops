0c027a96bb4fc99d015ae630159b87b641635843
shreyshah33 <shreyshah33@gmail.com>
2019-11-10 19:34:46 -0600

    working model

--- fall2019/fast_api/main.py
+++ fall2019/fast_api/main.py
@@ -1,8 +1,46 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+import datetime
+import json
 
 app = FastAPI()
 
 
+class Tweet(BaseModel):
+    message: str
+    owner: str
+
+
+def jsonify_tweet(tweet: Tweet):
+    return {
+        "owner": tweet.owner,
+        "message": tweet.message
+    }
+
+
+def write_tweet(tweet: Tweet):
+    data = {}
+    with open("tweets.json", "r") as file:
+        data = json.load(file)
+    with open("tweets.json", "w") as file:
+        data["tweets"].append(jsonify_tweet(tweet))
+        json.dump(data, file)
+
+
 @app.get("/")
-def read_root():
+def home():
     return {"Hello": "World"}
+
+
+@app.post("/tweets")
+def tweet_send(tweet: Tweet):
+    write_tweet(tweet)
+    return {"message": "Tweet recieved!"}
+
+
+@app.get("/tweets")
+def tweet_get():
+    data = {}
+    with open("tweets.json", "r") as file:
+        data = json.load(file)
+    return data
